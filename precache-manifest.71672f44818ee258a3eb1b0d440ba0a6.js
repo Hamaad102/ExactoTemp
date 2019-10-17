5e99cdc61734eba026c6f45415883a6e67438726
Hamaad Chughtai <Hamaad102@users.noreply.github.com>
2019-10-17 11:58:06 -0400

    Add files via upload

---
+++ precache-manifest.71672f44818ee258a3eb1b0d440ba0a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2dc1dcbf32cd4ec74b34",
+    "url": "/static/js/main.2dc1dcbf.chunk.js"
+  },
+  {
+    "revision": "2fa63c9cbd20d2d4541d",
+    "url": "/static/js/1.2fa63c9c.chunk.js"
+  },
+  {
+    "revision": "2dc1dcbf32cd4ec74b34",
+    "url": "/static/css/main.774fd504.chunk.css"
+  },
+  {
+    "revision": "d18c0f1d5bfde6bd2c4109efa3de4850",
+    "url": "/index.html"
+  }
+];
